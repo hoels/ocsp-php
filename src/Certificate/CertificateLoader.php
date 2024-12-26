d527d046140a4eb5f6040354e942713beb107855
hoels <51371415+hoels@users.noreply.github.com>
2024-12-26 22:31:27 +0100

    Adjust license

--- src/Certificate/CertificateLoader.php
+++ src/Certificate/CertificateLoader.php
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2022-2024 Estonian Information System Authority
+ * Copyright (c) 2024 Kai Hölscher
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
